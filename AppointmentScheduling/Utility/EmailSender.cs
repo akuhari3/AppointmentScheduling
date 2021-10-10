3adf2c3cd53c784a2f100fd9a6a760769ff3d5c0
Alen Kuharić <alen.kuharic@gmail.com>
2021-10-10 12:25:30 +0200

    Add project files.

---
+++ AppointmentScheduling/Utility/EmailSender.cs
@@ -0,0 +1,38 @@
+﻿using Mailjet.Client;
+using Mailjet.Client.Resources;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentScheduling.Utility
+{
+    public class EmailSender : IEmailSender
+    {
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            MailjetClient client = new MailjetClient("dd438c35aa524b556c7714c0ff9f915f", "cb33cda2c996bbcaff3cc013bb08036f")
+            {
+
+            };
+            MailjetRequest request = new MailjetRequest
+            {
+                Resource = Send.Resource,
+            }
+           .Property(Send.FromEmail, "alen.kuharic@gmail.com")
+           .Property(Send.FromName, "Appointment Scheduler")
+           .Property(Send.Subject, subject)
+           .Property(Send.HtmlPart, htmlMessage)
+           .Property(Send.Recipients, new JArray {
+                new JObject {
+                 {"Email", email}
+                 }
+               });
+            MailjetResponse response = await client.PostAsync(request);
+
+
+        }
+    }
+}
